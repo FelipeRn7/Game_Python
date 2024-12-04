aa05d7bbf58a12b23498fc01c2706cee72768334
Fe <felipenienow754@gmail.com>
2024-12-04 00:40:59 -0300

    Creating Players and Enemies/Meteors Moving players and Enemies/Meteors

--- code/Const.py
+++ code/Const.py
@@ -1,15 +1,22 @@
 # C
+import pygame
+
 COLOR_WHITE = (255, 255, 255)
 COLOR_GOLD = (238, 155, 45)
 COLOR_VIOLET = (147, 112, 219)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT
 ENTITY_SPEED = {
     'Level1bg0': 0,
     'Level1bg1': 1,
     'Level1bg2': 2,
     'Level1bg3': 3,
     'Level1bg4': 4,
+    'Player1': 3,
+    'Player2': 3,
+    'Meteor1': 2,
+    'Meteor2': 3,
 }
 
 # M
@@ -18,6 +25,18 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                "EXIT")
+
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
 
 # W
 WIN_WIDTH = 576
--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
+
 from code.Background import Background
-from code.Const import WIN_WIDTH
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Enemy import Enemy
+from code.Player import Player
 
 
 class EntityFactory:
@@ -15,3 +19,11 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player1':
+                return Player('Player1', (10, WIN_HEIGHT / 2 -30))
+            case 'Player2':
+                return Player('Player2', (10, WIN_HEIGHT / 2 +30))
+            case 'Meteor1':
+                return Enemy('Meteor1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT -40)))
+            case 'Meteor2':
+                return Enemy('Meteor2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT +40)))
